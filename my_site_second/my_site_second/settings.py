c300c12323952d8c147d0d2550269a8fb9755d79
SamVG <uladimir97@yandex.by>
2025-07-05 14:59:17 +0300

    заработпло с нвой базой данных і енв

--- my_site_second/my_site_second/settings.py
+++ my_site_second/my_site_second/settings.py
@@ -17,12 +17,13 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env=environ.Env().read_env(BASE_DIR / '.env')
+env=environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY =  env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
@@ -79,8 +80,14 @@ WSGI_APPLICATION = 'my_site_second.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST':env('POSTGRES_HOST'),
+        'PORT':env('POSTGRES_PORT')
+
+
     }
 }
 
